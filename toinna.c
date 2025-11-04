622b4b3f1b65bc221ad4e7d3a47ef5877750736d
Musfique Ahmed <mahmed2330101@bsds.uiu.ac.bd>
2025-11-05 02:58:36 +0600

    Update toinna.c

--- toinna.c
+++ toinna.c
@@ -1,11 +1,11 @@
-  #include <stdio.h>
-  #include <conio.h>
-    main()
-    {
-        int o;  
-        printf("enter the value of o :");
-        scanf("%d",o);
-        tg=o+o*3+(o*3)-5;
-        printf("the restlt=%d",tg);
-        getch();
-    }   
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+    int o;  
+    printf("enter the value of o :");
+    scanf("%d",o);
+    tg=o+o*3+(o*3)-5;
+    printf("the restlt=%d",tg);
+    getch();
+}
